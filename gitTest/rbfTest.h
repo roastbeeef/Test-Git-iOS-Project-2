aef8bd058a7577178ac6a8036c979f8287539fd8
matt <Matt@Users-MacBook-Pro.local>
2014-05-09 23:25:54 +0100

    added a test class

---
+++ gitTest/rbfTest.h
@@ -0,0 +1,13 @@
+//
+//  rbfTest.h
+//  gitTest
+//
+//  Created by matt on 09/05/2014.
+//  Copyright (c) 2014 matt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface rbfTest : NSObject
+
+@end
---
+++ gitTest/rbfTest.m
@@ -0,0 +1,13 @@
+//
+//  rbfTest.m
+//  gitTest
+//
+//  Created by matt on 09/05/2014.
+//  Copyright (c) 2014 matt. All rights reserved.
+//
+
+#import "rbfTest.h"
+
+@implementation rbfTest
+
+@end
