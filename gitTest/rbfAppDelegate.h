2fe1b852e66ab762588b6207c49ad4790e863270
matt <Matt@users-mbp.home>
2014-05-09 19:29:53 +0100

    Initial Commit

---
+++ gitTest/rbfAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  rbfAppDelegate.h
+//  gitTest
+//
+//  Created by matt on 09/05/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rbfAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitTest/rbfViewController.h
@@ -0,0 +1,13 @@
+//
+//  rbfViewController.h
+//  gitTest
+//
+//  Created by matt on 09/05/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rbfViewController : UIViewController
+
+@end
